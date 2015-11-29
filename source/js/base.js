1252ee48d0114776e9c63acee1e1fc425952ad23
Paulo Silva <engelik@gmail.com>
2015-11-29 21:57:05 +0000

    started game logic

--- source/js/base.js
+++ source/js/base.js
@@ -1,1 +1,30 @@
-console.log("Im working!");
+var main = function() {
+	// console.log($($('.game-left').children()[2]).addClass('test'));
+	var timerEl = $('.timer');
+	
+	// target number of updates per second:
+	var ticksPerSec = 10;
+	// time per game in seconds
+	var gameTime = 2; 
+	var timer = gameTime * ticksPerSec;
+	
+	var frameTime = 1000/ ticksPerSec;
+	var initTime;
+	var lastFrameTime;
+	var currentFrameTime;
+		
+	// Game loop: 
+	setInterval(function(){
+		if (timer >= 0){ 
+			timerEl.text(timer--);
+		}
+		else {
+			timerEl.text('Lost');
+		}
+	}, frameTime);
+	$('.bb').click(function(){
+		
+	});
+};
+
+$(document).ready(main);
